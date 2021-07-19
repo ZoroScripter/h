0dfb37b3135b596bdf84025f4114fac67bbb09c0
ZoroScripter <87605900+ZoroScripter@users.noreply.github.com>
2021-07-19 14:39:13 -0300

    h

---
+++ h.py
@@ -0,0 +1,238 @@
+import os
+os.system('clear')
+print('========================')
+nome = input('Digite Seu nome: \n')
+print('========================')
+os.system('clear')
+os.system('pip install requests')
+os.system('clear')
+
+print('░░░░░░░░░░░░▄▀▀▀▄░░░░░░')
+print(' ░░░░░░░▄███▀░◐░░░▌░░░░░')
+print(' ░░░░░░░░░░░▌░░░░░▐░░░░░')
+print('░░░░░░░░░░░▌░░░░▐░░░░░░')
+print(' ░░░░░░░░░░░█▀▀░░█░░░░░░')
+print(' ░░░░░░▄▀▀▀▀░░░░░█▄▄░░░░')
+print(' ░░░░░░█░█░░░░░░░░░░▐░░░')
+print(' ░░░░░░▐▐░░░░░░░░░▄░▐░░░')
+print(' ░░░░░░█░░░░░░░░▄▀▀░▐░░░')
+print(' ░░░░▄▀░░░░░░░░▐░▄▄▀░░░░')
+print('░░▄▀░░░▐░░░░░█▄▀░▐░░░░░')
+print(' ░░█░░░▐░░░░░░░░▄░█░░░░░')
+print('░░░█▄░░▀▄░░░░▄▀▐░█░░░░░')
+print('░░░█▐▀▀▀    ▀▀▀▀░░▐░█░░░░░')
+print('░░▐█▐▄░░░░░░░░░▐░█⠁Script By h4ck3r 777')
+print('=====================================')
+print (nome)
+print('Bem Vindo')
+print('Criador: h4ck3r 777')
+print('Parcerias: @Narutin_Das_Edit, ZEUS FF e Guilda Lordes')
+print('=====================================')
+print('')
+print('=====================================')
+print('1 #WebSploit')#FF1D00
+print('2 #ngrok')#FF57F8
+print('3 #Sqlmap')#FF57F8
+print('4 #Trojans')#FF57F8
+print('5 #LITESPAM')#FF57F8
+print('6 #DDos')#FF57F8
+print('7 #Black-Hydra')#FF57F8
+print('8 #Gato-Dark v1')#FF57F8
+print('9 #Cupp')#FF57F8
+print('10 #1000 inscritos por dia')#FF57F8
+print('11 #SocialSploit/hackear contas')#FF57F8
+print('12 #pegar o ip de vc msm')#FF57F8
+print('13 #Root termux')#FF57F8
+print('14 #FSciety')#FF57F8
+print('15 #Sniper')#FF57F8
+print('16 #mudar a cor das letras do termux')#FF57F8
+print('17 #Puxar dados')#FF57F8
+print('18 #Spayer')#FF57F8
+print('19 #wifite')#FF57F8
+print('20 #w3m entrar em sites')#FF57F8
+print('21 #Tool-X')#FF57F8
+print('22 #SMAP SMS')#FF57F8
+print('23 #LITEDDOS')#FF57F8
+print('24 #Hiddeneye')#FF57F8
+print('25 #Bot de figurinhas')#FF57F8
+print('26 Wali Canal')
+print('27 meu canal')
+print('00 #Exit')#FF57F8
+print('99 atualizar')#FF57F8
+print('====================================')
+op = int(input(' Digite uma das opções: '))#00E9FF
+print('=====================================')
+print('')
+if op == 1:
+	os.system('apt install git')
+	os.system('apt install python2')
+	os.system('pip2 install requests')
+	os.system('pip install --upgrade pip')
+	os.system('git clone https://github.com/websploit/websploit.git')
+	print('está ai os comandos')
+	print('=====================================')
+	print('===============================')
+if op == 2:
+	os.system('apt update && apt upgrade')
+	os.system('apt install git')
+	os.system(' git clone https://github.com/tchelospy/termux-ngrok.git ')
+	print('está ai os comandos')
+	print('=====================================')
+	print('===============================')
+if op == 3:
+	os.system('apt-get update')
+	os.system('apt-get install python2')
+	os.system('apt install git')
+	os.system('git clone https://github.com/sqlmapproject/sqlmap')
+	print('está ai os comandos')
+	print('=====================================')
+	print('===============================')
+if op == 4:
+	os.system('apt install git')
+	os.system('apt install python2')
+	os.system('git clone https://github.com/R133F/Trojans')
+	print('está ai os comandos')
+	print('=====================================')
+if op == 5:
+	os.system('pkg install python2')
+	os.system('pkg install curl')
+	os.system('pkg install php')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/4L13199/LITESPAM')
+	print('está ai os comandos')
+	print('=====================================')
+if op == 6:
+	os.system('pkg install python2')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/pembriahmad/DDOS')
+	print('está ai os comandos')
+	print('=====================================')
+if op == 7:
+	os.system('pkg install python2')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/Gameye98/Black-Hydra')
+	print('=====================================')
+	print('está ai os comandos')
+	print('===============================')
+if op == 8:
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install python2')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/Ranginang67/DarkFly-Tool')
+	print('=====================================')
+	print('está ai os comandos')
+if op == 9:
+	os.system('git clone https://github.com/Mebus/cupp')
+	os.system('cd cupp')
+	print('Dentro Da ferramenta coloque as informações da pessoa')
+	print('=====================================')
+if op == 10:
+	os.system('git clone https://github.com/nandy121/inshackle')
+	os.system('cd inshackle')
+	print('=====================================')
+if op == 11:
+	os.system('pkg install git -y')
+	os.system('git clone https://github.com/Cesar-Hack-Gray/SocialSploit')
+	print('=====================================')
+if op == 12:
+	os.system('Digite ifconfig')
+	os.system('se for no windows')
+	os.system('Digite ipconfig')
+	print('=====================================')
+if op == 13:
+	os.system('apt update && apt upgrade')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/Anonymous-Zpt//T-root')
+	print('===============================')
+if op == 14:
+	os.system('apt update && apt upgrade')
+	os.system('pkg install python2')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/Manisso/fsociety')
+	print('===============================')
+if op == 15:
+	os.system('pkg install git')
+	os.system('git clone https://github.com/1N3/Sn1per')
+	print('===============================')
+if op == 16: 
+	os.system('apt update')
+	os.system('apt upgrade')
+	os.system('pkg install git')
+	os.system('pkg install python')
+	os.system('git clone https://github.com/mf256010/Corlux')
+	print('===============================')
+if op == 17:
+	os.system('pkg update && pkg upgrade')
+	os.system('pkg install python')
+	os.system('pkg install git')
+	os.system('pip install requests')
+	os.system('git clone https://github.com/Kiny-Kiny/Kiny-Painel')
+	print('===============================')
+if op == 18:
+	os.system('pkg install git')
+	os.system('git clone https://github.com/Cesar-Hack-Gray/Spayer')
+	print('===============================')
+if op == 19:
+	os.system('apt update && apt upgrade')
+	os.system('pkg install python2')
+	os.system('pkg install git')
+	os.system('git clone https://github.com/derv82/wifite')
+	print('===============================')
+if op == 20:
+	os.system('pkg install w3m')
+	print('w3m mais o site')
+	print('para sair, volume (+) + (z)')
+	print('===============================')
+if op == 21:
+	os.system('apt update && apt upgrade')
+	os.system('git clone https://github.com/rajkumardusad/Tool-X.git')
+	print('===============================')
+if op == 22:
+	os.system('apt update && apt upgrade')
+	os.system('pkg install python')
+	os.system('pkg install git')
+	os.system('python3 -m pip install requests')
+	os.system('git clone https://github.com/sandiwijayani1/SpamWa')
+	print('===============================')
+if op == 23:
+	os.system('apt update && apt upgrade')
+	os.system('apt install git')
+	os.system('pkg install python2')
+	os.system('git clone https://github.com/4L13199/LITEDDOS')
+	print('===============================')
+if op == 24:
+	os.system('pkg install git python php curl openssh grep -y')
+	os.system('git clone https://github.com/DarkSecDevelopers/HiddenEye.git')
+	os.system('chmod 777 HiddenEye')
+	os.system('cd HiddenEye')
+	os.system(' pip install -r requeriments.txt')
+	os.system('pip install requests')
+	print('===============================')
+if op == 25:
+	os.system('pkg install nojets -y')
+	os.system('git clone https://github.com/fdciabdul/termux-whatsapp-bot')
+	print('===============================')
+if op == 26:
+	os.system('xdg-open https://youtube.com/channel/UC_QXCwWs7bAQwTctPUFtntg')
+	os.system('python3 h.py')
+if op == 27:
+	os.system('xdg-open https://youtube.com/channel/UCfswyN1ty2no6qZw4WvWSlw')
+	os.system('python3 h.py')
+if op == 00:
+	os.system('clear')
+	print('=============================')
+	print('h4ck3r 777 se despedindo de vc tmj')
+	print('passa la no meu canal:')
+	print('my canal:https://youtube.com/channel/UClGvVtBwhb0OeIckDZ8kP_Q')
+	print('se quiser aprender a como que faz um Script me fala que eu ensino tmj')
+	print('==============================')
+	exit()
+if op == 99:
+	os.system('clear')
+	os.system('python3 h.py')
+print('====================================')
+print('criador: h4ck3r 777')
+print('Parcerias: Narutin Das edit e ZEUS FF, Guilda Lordes')
+print('O Script Atualiza Todo Dia então todo dia tem novos Scripts')
+print('=====================================')
+print('')
